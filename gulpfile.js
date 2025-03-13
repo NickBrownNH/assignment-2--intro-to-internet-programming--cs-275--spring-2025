f782c80dff6b888479e5a0a3048f5732a24d6a61
Nicholas Brown <nb@brownsfromsa.com>
2025-03-12 23:15:38 -0400

    Added Working Carousel through js and css, and manipulated json file to work as jsonp file
    ***NOTE***
    I had to change the names of the images to include underscores in with them because I was getting a weird bug where once I compiled and ran the code through to the browser, the image sources would change to the image names but with the underscores and the browser would say it didn't find the images. I spent the majority of my time working on this bug, trying to figure out what was causing it, seeing if it was one of the gulp libraries, but the only thing I could find out is that it was most likely occurring somewhere after sending the code through to the browser serve. After changing the image names to what the browser was telling me it saw, and changing the image sources in the css file, it finally worked. I tested it out with different names and it seemed to only do this with a string similar to the image names'. I'm guessing this was some type of formatting thing where the image names inside the css would change to a formatted version, and the formatted version would have the underscore for some reason. This is why I needed to change the images names and the .gitignore. If this is reason for point deduction, I apologize, I tried everything I could think of and didn't find anything online that could help me in this situation.

--- gulpfile.js
+++ gulpfile.js
@@ -88,6 +88,7 @@ let serve = () => {
                 `js`,
                 `styles`,
                 `temp`,
+                `img`,
                 `.`
             ]
         }
--- js/main.js
+++ js/main.js
@@ -1,32 +1,141 @@
-// -----------------------------------------------------------------------------
-// This file includes deliberate formatting errors in order for you to verify
-// that ESLint and EditorConfig are working properly. If both tools are, indeed,
-// working correctly, then you’d see errors in your editor about indentation and
-// improper use of footmarks instead of back ticks. When you save this file,
-// your editor should strip all excess newlines and whitespace characters from
-// the file. If both of these events occur, then ESLint and EditorConfig are
-// working correctly.
-//
-// DON’T PROCEED UNTIL YOU’RE SURE ESLINT AND EDITORCONFIG ARE WORKING CORRECTLY
-// -----------------------------------------------------------------------------
-window.onload = () => {
-       alert('Test');
+let body = document.querySelector(`body`);
+let albumNames = document.getElementsByClassName(`album-names`);
+let artistNames = document.getElementsByClassName(`artist-names`);
+let imageCredits = document.getElementsByClassName(`image-credits`);
+let reviewContents = document.getElementsByClassName(`review-contents`);
+let reviewCredits = document.getElementsByClassName(`review-credits`);
+
+let pos = -20;
+
+window.onload = function() {
+    items();
+};
+
+function items(data) {
+    updateArrows();
+    for (let i = 0; i < albumNames.length; i++) {
+        albumNames[i].textContent = data.albums[i].album;
+        artistNames[i].textContent = data.albums[i].artist;
+        artistNames[i].href = data.albums[i].url;
+
+        let creditText = document.createTextNode(`Credit: `);
+        imageCredits[i].parentNode.insertBefore(creditText, imageCredits[i]);
+
+        imageCredits[i].textContent = data.albums[i].cover_image.credit;
+        imageCredits[i].href = data.albums[i].cover_image.url;
+
+        reviewContents[i].textContent = data.albums[i].review.content;
+        reviewCredits[i].textContent = `-` + data.albums[i].review.source;
+        reviewCredits[i].href = data.albums[i].review.url;
+
+        let albumPhoto = document.querySelector(
+            `.carousel-slides > div:nth-child(${i + 1}) > .album-image`
+        );
+        albumPhoto.style.backgroundImage = `url('${data.albums[i].cover_image.path}')`;
+    }
 }
 
+document.addEventListener(`keydown`, function(event) {
+    if (event.keyCode === 37 && pos != -2060) {
+        pos = pos - 680;
+        document.documentElement.style.setProperty(`--slides-position`, pos + `px`);
+        updateArrows();
+    }
+    if (event.keyCode === 39 && pos != -20) {
+        pos = pos + 680;
+        document.documentElement.style.setProperty(`--slides-position`, pos + `px`);
+        updateArrows();
+    }
+});
 
+let leftArrow = document.querySelector(`nav a:nth-child(1)`);
+leftArrow.addEventListener(`click`, function() {
+    if (pos != -2060) {
+        leftArrow.style.display = `inline-block`;
+        pos = pos - 680;
+        document.documentElement.style.setProperty(`--slides-position`, pos + `px`);
+    }
+    updateArrows();
+});
 
+let rightArrow = document.querySelector(`nav a:nth-child(2)`);
+rightArrow.addEventListener(`click`, function() {
+    if (pos != 20) {
+        pos = pos + 680;
+        document.documentElement.style.setProperty(`--slides-position`, pos + `px`);
+    }
+    updateArrows();
+});
 
+function updateArrows() {
+    if (pos == -20) {
+        rightArrow.style.visibility = `hidden`;
+    } else {
+        rightArrow.style.visibility = `visible`;
+    }
 
+    if (pos == -2060) {
+        leftArrow.style.visibility = `hidden`;
+    } else {
+        leftArrow.style.visibility = `visible`;
+    }
+}
 
+for (let i = 0; i < 4; i++) {
+    updateArrows();
 
+    let carouselSlidesPosition = document.querySelector(`.carousel-slides`);
 
+    let albumDivider = document.createElement(`div`);
+    albumDivider.setAttribute(`class`, `album`);
+    carouselSlidesPosition.appendChild(albumDivider);
 
+    let topHeaders = document.createElement(`header`);
+    albumDivider.appendChild(topHeaders);
 
+    let albumNameHeader = document.createElement(`h2`);
+    albumNameHeader.setAttribute(`class`, `album-names`);
+    topHeaders.appendChild(albumNameHeader);
 
+    let artistNameHeader = document.createElement(`h3`);
+    topHeaders.appendChild(artistNameHeader);
 
+    let artistAnchor = document.createElement(`a`);
+    artistAnchor.href = `filler`;
+    artistAnchor.classList.add(`artist-names`);
+    artistNameHeader.appendChild(artistAnchor);
 
+    let imageDivider = document.createElement(`div`);
+    imageDivider.setAttribute(`class`, `album-image`);
+    albumDivider.appendChild(imageDivider);
 
+    let creditHeader = document.createElement(`header`);
+    albumDivider.appendChild(creditHeader);
 
+    let imageCreditsHeader = document.createElement(`h4`);
+    creditHeader.appendChild(imageCreditsHeader);
 
+    let imageCreditsAnchor = document.createElement(`a`);
+    imageCreditsAnchor.href = `filler`;
+    imageCreditsAnchor.classList.add(`image-credits`);
+    imageCreditsHeader.appendChild(imageCreditsAnchor);
 
+    let reviewParagraph = document.createElement(`p`);
+    reviewParagraph.classList.add(`review-contents`);
+    albumDivider.appendChild(reviewParagraph);
 
+    let bottomHeader = document.createElement(`header`);
+    albumDivider.appendChild(bottomHeader);
+
+    let reviewCreditsHeader = document.createElement(`h5`);
+    bottomHeader.appendChild(reviewCreditsHeader);
+
+    let reviewCreditsAnchor = document.createElement(`a`);
+    reviewCreditsAnchor.href = `filler`;
+    reviewCreditsAnchor.classList.add(`review-credits`);
+    reviewCreditsHeader.appendChild(reviewCreditsAnchor);
+}
+
+let script = document.createElement(`script`);
+script.setAttribute(`src`, `json/data.json`);
+body.appendChild(script);
